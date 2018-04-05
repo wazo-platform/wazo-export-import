cffaf7cf2a6f4cd484afaddfcf5ada3665247eb0
Pascal Cadotte Michaud <pcm@wazo.io>
2018-04-05 10:16:26 -0400

    output data as json

--- export.py
+++ export.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python
 
+import csv
+import json
+from StringIO import StringIO
+
 from xivo_auth_client import Client as Auth
 from xivo_confd_client import Client as Confd
-from pprint import pprint
 
-
-username = "sylvain" 
-password = "sylvain"
+username = "test"
+password = "test"
 backend = "xivo_service"
-server = "localhost"
+server = "10.37.0.254"
+
 
 def get_token():
     auth = Auth(server, username=username, password=password, verify_certificate=False)
     token_data = auth.token.new(backend, expiration=180)
     return token_data['token']
+
 
 def get_confd(token):
     return Confd(server, verify_certificate=False, token=token)
@@ -21,9 +25,19 @@ def get_confd(token):
 token = get_token()
 confd = get_confd(token)
 
-pprint(confd.entities.list())
-pprint(confd.contexts.list())
-pprint(confd.groups.list())
-pprint(confd.pagings.list())
-pprint(confd.incalls.list())
-print confd.users.export_csv()
+users = StringIO(confd.users.export_csv())
+reader = csv.DictReader(users, delimiter=',', quotechar='"')
+exported_users = [row for row in reader]
+
+print json.dumps(
+    {
+        'entities': confd.entities.list(),
+        'contexts': confd.contexts.list(),
+        'devices': confd.devices.list(),
+        'groups': confd.groups.list(),
+        'pagings': confd.pagings.list(),
+        'incalls': confd.incalls.list(),
+        'lines': confd.lines.list(),
+        'users': exported_users,
+    }
+)
