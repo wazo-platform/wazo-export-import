0d523e7d7eddf1487bec22ff9366905a33739db4
Pascal Cadotte Michaud <pcm@wazo.io>
2021-04-07 14:26:06 -0400

    user export: add fallbacks

--- wazo_export_import/helpers/constants.py
+++ wazo_export_import/helpers/constants.py
@@ -3,6 +3,10 @@
 
 RESOURCE_FIELDS = {
     # SELECT uuid as ref, rightcallcode as call_permission_password, enablexfer as call_transfer_enabled, true as dtmf_hangup_enabled, email, destunc as unconditional_forward_destination, enableunc as unconditional_forward_enabled, destbusy as busy_forward_destination, enablebusy as busy_forward_enabled, destrna as no_answer_forward_destination, enablerna as no_answer_forward_enabled, firstname, lastname, language, mobilephonenumber as mobile_phone_number, outcallerid as outgoing_caller_id, passwdclient as password, callrecord as recording_incoming_external_enabled, callrecord as recording_incoming_internal_enabled, callrecord as recording_outgoing_external_enabled, callrecord as recording_outgoing_internal_enabled, ringseconds as  ring_seconds, simultcalls as simultaneous_calls, 1 as subscription_type, enablehint as supervision_enabled, userfield as userfield, loginclient as username, description, musiconhold as music_on_hold, preprocess_subroutine, callerid as caller_id, timezone FROM userfeatures;
+    # select userfeatures.uuid as ref, case when dialaction.action = 'voicemail' then concat('vm-', dialaction.actionarg1) end as fallback_no_answer, dialaction.actionarg2 as fallback_no_answer_argument from dialaction join userfeatures on userfeatures.id = cast(dialaction.categoryval as int) and category = 'user' where category = 'user' and action  != 'none' and linked='1' and event='noanswer';
+    # select userfeatures.uuid as ref, case when dialaction.action = 'voicemail' then concat('vm-', dialaction.actionarg1) end as fallback_busy, dialaction.actionarg2 as fallback_busy_argument from dialaction join userfeatures on userfeatures.id = cast(dialaction.categoryval as int) and category = 'user' where category = 'user' and action  != 'none' and linked='1' and event='busy';
+    # select userfeatures.uuid as ref, case when dialaction.action = 'voicemail' then concat('vm-', dialaction.actionarg1) end as fallback_congestion, dialaction.actionarg2 as fallback_congestion_argument from dialaction join userfeatures on userfeatures.id = cast(dialaction.categoryval as int) and category = 'user' where category = 'user' and action  != 'none' and linked='1' and event='congestion';
+    # select userfeatures.uuid as ref, case when dialaction.action = 'voicemail' then concat('vm-', dialaction.actionarg1) end as fallback_fail, dialaction.actionarg2 as fallback_fail_argument from dialaction join userfeatures on userfeatures.id = cast(dialaction.categoryval as int) and category = 'user' where category = 'user' and action  != 'none' and linked='1' and event='chanunavail';
     "users": {
         "fields": {
             "call_permission_password": {},
@@ -13,6 +17,11 @@ RESOURCE_FIELDS = {
             "fallback_congestion": {},  # Dialaction
             "fallback_fail": {},  # Dialaction
             "fallback_no_answer": {},  # Dialaction
+            # The arguments for fallback depends on the destination type
+            "fallback_busy_argument": {},  # Dialaction
+            "fallback_congestion_argument": {},  # Dialaction
+            "fallback_fail_argument": {},  # Dialaction
+            "fallback_no_answer_argument": {},  # Dialaction
             "unconditional_forward_destination": {},
             "unconditional_forward_enabled": {},
             "busy_forward_destination": {},
